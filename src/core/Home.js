d90ba3c2123afee52703f2bc8009bc74bdf3fe90
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-22 22:45:06 +0530

    [Routes] signup signin

--- src/core/Home.js
+++ src/core/Home.js
@@ -6,7 +6,7 @@ import Base from "./Base";
 const Home = () => {
     console.log('API Is', API);
     return (
-        <Base>
+        <Base title="Home Page" description="Welcome to t-shirt store">
             <h1 className="text-white">Hello front-end</h1>
         </Base>
     )
---
+++ src/user/Signup.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+    return (
+        <Base title="SignUp Page" description="A page for user to signup">
+            <h1>SignUp Works!</h1>
+        </Base>
+    )
+};
+
+export default Signup;
