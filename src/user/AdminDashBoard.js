3bb7d5d7a4ca269376e96d513eb3d1f9bf613569
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-24 14:06:10 +0530

    admin panel

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -1,10 +1,78 @@
 import React from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
+
+    const { user: { name, email, role } } = isAuthenticated();
+
+    const adminLeftSide = () => {
+        return (
+            <div className="card">
+                <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link className="nav-link text-success" to="/admin/create/category">
+                            Create Categories
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link text-success" to="/admin/create/product">
+                            Create Products
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link text-success" to="/admin/products">
+                            Manage Products
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link text-success" to="/admin/orders">
+                            Manage Orders
+                        </Link>
+                    </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link text-success" to="/admin/create/category">
+                            Create Categories
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    };
+
+    const adminRightSide = () => {
+        return (
+            <div className="card mb-4">
+                <div className="card-header">
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <span className="badge badge-success mr-2">Name:</span>{name}
+                        </li>
+                        <li className="list-group-item">
+                            <span className="badge badge-success mr-2">Email:</span>{email}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    };
+
     return (
-        <Base title="Admin Dashboard Page">
-            <h1>This is a admin dashboard page</h1>
+        <Base
+            title="Welcome to admin area "
+            description="Manage all your products here"
+            className="container bg-success p-4"
+        >
+            <div className="row">
+                <div className="col-3">
+                    {adminLeftSide()}
+                </div>
+                <div className="col-9">
+                    {adminRightSide()}
+                </div>
+            </div>
         </Base>
     )
 };
