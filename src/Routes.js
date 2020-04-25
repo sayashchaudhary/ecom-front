bcc47cda52c14c31c747fd9a032ce020f1d349c2
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-25 13:50:10 +0530

    create categories

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './core/Home';
+import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import AdminRoutes from "./auth/helper/AdminRoutes";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashboard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
 
 const Routes = () => {
     return (
@@ -17,6 +18,7 @@ const Routes = () => {
                 <Route path='/signin' exact component={Signin}/>
                 <PrivateRoutes path='/user/dashboard' exact component={UserDashBoard}/>
                 <AdminRoutes path='/admin/dashboard' exact component={AdminDashboard}/>
+                <AdminRoutes path='/admin/create/category' exact component={AddCategory}/>
             </Switch>
         </BrowserRouter>
     )
