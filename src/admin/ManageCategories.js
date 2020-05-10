d05e972c8767ca884afce7a1bd9e59111503971e
sayash chaudhary <sayashchaudhary@gmail.com>
2020-05-11 01:09:01 +0530

    manage categories

--- src/admin/ManageCategories.js
+++ src/admin/ManageCategories.js
@@ -1,10 +1,80 @@
-import React from "react";
+//TODO: complete it
+//same as manage product and update product
+
+import React, { useEffect, useState } from "react";
 import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
+import { deleteProduct, getCategoies } from "./helper/adminapicall";
 
 const ManageCategories = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const { user, token } = isAuthenticated();
+
+    const preload = () => {
+        getCategoies().then(data => {
+            if (data?.error) {
+                console.log(data.error)
+            } else {
+                setCategories(data);
+            }
+        })
+    };
+
+    useEffect(() => {
+        preload()
+    }, []);
+
+    const deleteThisCategory = categoryId => {
+        deleteProduct(categoryId, user._id, token)
+            .then(data => {
+                if (data?.error) {
+                    console.log(data.error)
+                } else {
+                    preload()
+                }
+            })
+    };
+
     return(
-        <Base>
-            <h1 className="text-white">Assignment</h1>
+        <Base title="Welcome admin" description="Manage products here">
+            <h2 className="mb-4">All products:</h2>
+            <Link className="btn btn-info" to={`/admin/dashboard`}>
+                <span className="">Admin Home</span>
+            </Link>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="text-center text-white my-3">Total {categories.length} products</h2>
+                    {categories.map((category, index) => {
+                        return (
+                            <div key={index} className="row text-center mb-2 ">
+                                <div className="col-12">
+
+                                </div>
+                                <div className="col-4">
+                                    <h3 className="text-white text-left">{category.name}</h3>
+                                </div>
+                                <div className="col-4">
+                                    <Link className="btn btn-success"
+                                          to={`/admin/category/update/${category._id}`}
+                                    >
+                                        <span className="">Update</span>
+                                    </Link>
+                                </div>
+                                <div className="col-4">
+                                    <button onClick={() => {
+                                        deleteThisCategory(category._id)
+                                    }} className="btn btn-danger">
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </Base>
     )
 };
