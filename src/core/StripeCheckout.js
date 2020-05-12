c362c9a3c79636822b7ca35cd683d4f22e966668
sayash chaudhary <sayashchaudhary@gmail.com>
2020-05-12 16:31:46 +0530

    payment gateway

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "./helper/cartHelper";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
 
 const StripeCheckout = ({ products, setReload = f => f, reload = undefined }) => {
 
@@ -23,9 +26,38 @@ const StripeCheckout = ({ products, setReload = f => f, reload = undefined }) =>
         return amount
     };
 
+    const makePayment = (token) => {
+        const body = {
+            token,
+            products
+        };
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+        return fetch(`${API}/stripepayment`, {
+            method: "POST",
+            headers,
+            body: JSON.stringify(body)
+        })
+            .then(res => {
+                console.log(res);
+                const { status } = res;
+                console.log('STATUS', status);
+            })
+            .catch(err => console.log(err));
+    };
+
     const showStripeButton = () => {
         return isAuthenticated() ? (
-            <button className="btn btn-success">Pay with strip</button>
+            <StripeCheckoutButton token={makePayment}
+                                  stripeKey="pk_test_EKGbt5ftzHUH6gBFhNaSAdsl00TMBxn4B6"
+                                  amount={getFinalPrice() * 100}
+                                  name="Buy T-Shirts"
+                                  shippingAddress
+                                  billingAddress
+            >
+                <button className="btn btn-success">Pay with strip</button>
+            </StripeCheckoutButton>
         ) : (
             <Link to='/signin'>
                 <button className="btn btn-warning">Signin</button>
